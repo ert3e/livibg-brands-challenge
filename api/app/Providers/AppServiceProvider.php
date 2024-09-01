f68fef704902765f2b1616a0037f0010b30cecf1
Dmytro Samoilenko <dmytro.php.jwz@gmail.com>
2024-09-01 13:12:15 +0300

    mqtt add interfaces for provider

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Services\Mqtt\ApiMqttListener;
+use App\Services\Mqtt\ApiMqttListenerInterface;
+use App\Services\Mqtt\ApiMqttPublisher;
+use App\Services\Mqtt\ApiMqttPublisherInterface;
 use App\Services\TvShowService;
 use App\Services\TvShowServiceInterface;
 use Illuminate\Support\ServiceProvider;
@@ -14,6 +18,8 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(TvShowService::class, TvShowServiceInterface::class);
+        $this->app->bind(ApiMqttListener::class, ApiMqttListenerInterface::class);
+        $this->app->bind(ApiMqttPublisher::class, ApiMqttPublisherInterface::class);
     }
 
     /**
